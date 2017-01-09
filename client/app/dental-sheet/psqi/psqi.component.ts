4bb508f3aae4f82a9869a63c4246c96ba38e0468
Macdows <malo.camaret@gmail.com>
2017-01-09 12:32:59 +0100

    Add more fields to PSQI form

--- client/app/dental-sheet/psqi/psqi.component.ts
+++ client/app/dental-sheet/psqi/psqi.component.ts
@@ -6,10 +6,13 @@ const uiRouter = require('angular-ui-router');
 import routes from './psqi.routes';
 
 export class PsqiComponent {
+  history = []
   textInputs = []
+  radioInputs = []
 
   /*@ngInject*/
   constructor() {
+    this.history = ['Pas au cours du dernier mois', 'Moins d\'1 fois par semaine', '1 ou 2 fois par semaine', '3 ou 4 fois par semaine'];
 
     this.textInputs = [
       {
@@ -31,6 +34,29 @@ export class PsqiComponent {
         label: '4. Au cours du mois dernier, combien d\'heures de sommeil effectif avez-vous eues chaque nuit ? (Ce nombre peut être différent du nombre d\'heures que vous avez passées au lit) ?',
         textBefore: 'Nombre d\'heures de sommeil par nuit : ',
         textAfter: false
+      }
+    ]
+
+    this.radioInputs = [
+      {
+        label: '6. Au cours du mois dernier, comment évalueriez-vous globalement la qualité de votre sommeil ?',
+        scale: ['Très mauvaise', 'Assez mauvaise', 'Assez bonne', 'Très bonne']
+      },
+      {
+        label: '7. Au cours du mois dernier, combien de fois avez-vous pris des médicaments (prescrits par votre médecin ou achetés sans ordonnance) pour faciliter votre sommeil ?',
+        scale: this.history
+      },
+      {
+        label: '8. Au cours du mois dernier, combien de fois avez-vous eu des difficultés à demeurer éveillé(e) pendant que vous conduisiez, preniez vos repas, étiez occupé(e) dans une activité sociale ?',
+        scale: this.history
+      },
+      {
+        label: '9. Au cours du mois dernier, à quel degré cela a-t-il représenté un problème pour vous d\'avoir assez d\'enthousiasme pour faire ce que vous aviez à faire ?',
+        scale: ['Pas de problème du tout', 'Seulement un tout petit problème', 'Un certain problème', 'Un très gros problème' ]
+      },
+      {
+        label: '10. Avez vous un conjoint ou un camarade de chambre ?',
+        scale: ['Ni l\'un, ni l\'autre', 'Oui, mais dans une chambre différente', 'Oui, dans la même chambre mais pas dans le même lit', 'Oui, dans le même lit']
       }
     ]
   }
