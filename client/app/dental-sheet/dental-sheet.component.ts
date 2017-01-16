f2830777bfc2e6bbbe4c11760ff36337fe2e9508
Guillaume Masson <Guillomemasson@gmail.com>
2017-01-16 14:17:16 +0100

    kek

--- client/app/dental-sheet/dental-sheet.component.ts
+++ client/app/dental-sheet/dental-sheet.component.ts
@@ -11,6 +11,9 @@ import EdasComponent from './edas/edas.component';
 export class DentalSheetComponent {
   /*@ngInject*/
   constructor() {
+
+      this.numbers = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10];
+      this.numbers2 = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10];
   }
 }
 
